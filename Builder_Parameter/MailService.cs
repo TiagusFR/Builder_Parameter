83c9a4c5d3b9d85120636f2e6024372fd14f9896
Tiago França <t.iago_196@hotmail.com>
2024-07-29 13:15:52 -0300

    Adicionar arquivos de projeto.

---
+++ Builder_Parameter/MailService.cs
@@ -0,0 +1,50 @@
+﻿using Builder_Parameter;
+
+public class MailService
+{
+    // Nested EmailBuilder class
+    public class EmailBuilder
+    {
+        private readonly Email email;
+        public EmailBuilder(Email email) => this.email = email;
+
+        // Fluent members
+        public EmailBuilder From(string from)
+        {
+            email.From = from;
+            return this;
+        }
+
+        public EmailBuilder To(string to)
+        {
+            email.To = to;
+            return this;
+        }
+
+        public EmailBuilder Subject(string subject)
+        {
+            email.Subject = subject;
+            return this;
+        }
+
+        public EmailBuilder Body(string body)
+        {
+            email.Body = body;
+            return this;
+        }
+    }
+
+
+    private void SendEmailInternal(Email email)
+    {
+
+        Console.WriteLine($"Sending email to: {email.To}, subject: {email.Subject}, body: {email.Body}");
+    }
+
+    public void SendEmail(Action<EmailBuilder> builder)
+    {
+        var email = new Email();
+        builder(new EmailBuilder(email));
+        SendEmailInternal(email);
+    }
+}
---
+++ Builder_Parameter/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Builder_Parameter
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var mailService = new MailService();
+            mailService.SendEmail(builder =>
+            {
+                builder.From("sender@microsoft.com")
+                .To("receiver@microsoft.com")
+                .Subject("Subject")
+                .Body("Hello world");
+            });
+        }
+    }
+}
